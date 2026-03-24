4d862138c40cbe24051d66e38bce2f55959b247c
Tanish-Panwar <tanishpanwarrajput@gmail.com>
2026-03-24 14:39:58 +0530

    bug fix

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
-const dns = require('dns');
-
-// Force IPv4 first to avoid ENETUNREACH on Render
-dns.setDefaultResultOrder('ipv4first');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: true }, // required for Supabase
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+  family: 4, 
 });
 
 module.exports = pool;
