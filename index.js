d38ec851be8353380dd9b97dc39a0cdc8dcf0446
Tanish-Panwar <tanishpanwarrajput@gmail.com>
2026-03-24 12:23:28 +0530

    Day 7 Ready for deployment

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const router = require('./routes/userRoutes');
 const app = express();
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(logger);
 app.use(errorHandler);
 app.use('/users', router);
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if(!err) console.log("server running");
     else console.log(err.message);
 })
