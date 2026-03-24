736f71ddac79961bdfe4dbb9e10ffe6aff4ade52
Tanish-Panwar <tanishpanwarrajput@gmail.com>
2026-03-24 13:53:16 +0530

    day 7 update commit fix bug

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,           // 'postgres'
+  host: process.env.DB_HOST,           // 'db.lgugpaggpuwyymvakwiz.supabase.co'
+  database: process.env.DB_NAME,       // 'postgres'
+  password: process.env.DB_PASSWORD,   // raw password, no encoding needed
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: true },   // for Supabase
 });
 
 module.exports = pool;
