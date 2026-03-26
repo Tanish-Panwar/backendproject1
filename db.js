2fe8252405c3285b38331106828f2b221891b09d
Tanish-Panwar <tanishpanwarrajput@gmail.com>
2026-03-26 13:14:52 +0530

    Day 9 Connection Pooling

--- db.js
+++ db.js
@@ -2,10 +2,29 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, 
-  },
-  family: 4, 
+  ssl: { rejectUnauthorized: false },
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+  family: 4,
 });
 
-module.exports = pool;
+// LOGGING aquisition, and release.
+pool.on('acquire', () => console.log('📥 Connection acquired'));
+pool.on('remove', () => console.log('🔴 Connection removed'));
+
+// Wrapped query
+const query = async (text, params) => {
+  const client = await pool.connect();
+  console.log('📥 Acquired connection');
+
+  try {
+    const res = await client.query(text, params);
+    return res;
+  } finally {
+    client.release();
+    console.log('📤 Released connection');
+  }
+};
+
+module.exports = { pool, query };
