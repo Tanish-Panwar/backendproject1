f11ffd5810717af6823e269eb62872ad4b63070b
Tanish-Panwar <tanishpanwarrajput@gmail.com>
2026-03-27 12:51:11 +0530

    Day 10 Rate Limiter using Slding Window + Redis

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,26 @@
+const redisClient = require('../config/redis');
+const LIMIT = 100;
+const WINDOW = 60;
+
+exports.rateLimiter = async (req, res, next) => {
+    try {
+        const userId = req.user?.id || req.ip;
+        const key = `rate:${userId}`;
+
+        const requests = await redisClient.incr(key);
+        if(requests === 1) {
+            await redisClient.expire(key, WINDOW);
+        }
+
+        if(requests > LIMIT) {
+            return res.status(429).json({
+                success: false,
+                message: "Too many requests. Try again later."
+            })
+        }
+        next();
+    } catch (error) {
+        console.error("Rate Limiter Error: ", error);
+        next();
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
 const authMiddleware = require('../middleware/auth');
+const { rateLimiter } = require('../middleware/rateLimiter');
 
 // Routes...
 
 router.get('/', controller.login);
 router.post('/', controller.register);
-router.get('/me', authMiddleware, controller.userInfo);
-router.get('/all', authMiddleware, controller.getUsers);
+router.get('/me', authMiddleware, rateLimiter, controller.userInfo);
+router.get('/all', authMiddleware, rateLimiter, controller.getUsers);
 
 module.exports = router;
